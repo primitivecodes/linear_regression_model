f90fce207c9d37fd43079173889d382157d28c46
primitivecodes <p.ufitinema@alustudent.com>
2026-07-23 21:54:36 +0200

    Update model.py, move best_model.pkl, update notebook

--- summative/API/app/model.py
+++ summative/API/app/model.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import joblib
 
-MODEL_PATH = Path(__file__).resolve().parents[1] / "best_model.pkl"
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+MODEL_PATH = BASE_DIR / "linear_regression" / "best_model.pkl"
+
+print("Loading model from:", MODEL_PATH)
 
 model = joblib.load(MODEL_PATH)
