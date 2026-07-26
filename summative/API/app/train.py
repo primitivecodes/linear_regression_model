fe9922847f0c89deb0979e4462e4356ad74914fe
primitivecodes <p.ufitinema@alustudent.com>
2026-07-26 21:11:10 +0200

    Store training data with API application

--- summative/API/app/train.py
+++ summative/API/app/train.py
@@ -1,26 +1,27 @@
+from pathlib import Path
 import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent
+
+DATA_PATH = BASE_DIR / "data" / "yield_df.csv"
+
 
 def retrain_model(file):
 
     new_data = pd.read_csv(file.file)
 
-    # Load old dataset
-    old_data = pd.read_csv("app/data/yield_df.csv")
+    old_data = pd.read_csv(DATA_PATH)
 
     combined = pd.concat(
         [old_data, new_data],
         ignore_index=True
     )
 
-    # Save updated dataset
-    combined.to_csv(
-        "app/data/yield_df.csv",
-        index=False
-    )
+    combined.to_csv(DATA_PATH, index=False)
 
     # TODO:
-    # Repeat preprocessing
-    # Train best model
-    # Save best_model.pkl
+    # preprocess
+    # retrain model
+    # save best_model.pkl
 
     return len(new_data)
